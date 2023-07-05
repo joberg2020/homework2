1058497d0193e2fc74e7a2cf4e6186231ac6813b
Eliassj <elias.sjolin@gmail.com>
2023-07-05 20:34:44 +0200

    [Lade till addition och subtraktion]

--- hw2.py
+++ hw2.py
@@ -10,8 +10,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class Interval:
+class interval:
 
     def __init__(self, lowest: float, highest: float):
         self._lowest = lowest
         self._highest = highest
+        
+    def __add__(self, other): # Add method
+        # Check if other is numeric
+        if isinstance(other, (int, float, interval)) == False:
+            raise TypeError("Can't add non-numeric")
+        sh = self._highest
+        sl = self._lowest
+        if isinstance(other, (int, float)):
+            return interval(sl + other, sh + other)
+        elif isinstance(other, interval):
+            oh = other._highest
+            ol = other._lowest
+            return interval(sl + ol, sh + oh)
+        
+    def __radd__(self, other): # Funkar inte, varför?
+        return self + other
+    
+    def __sub__(self, other): # Subtract method
+        # Check if other is numeric
+        if isinstance(other, (int, float, interval)) == False:
+            raise TypeError("Can't subtract non-numeric")
+        sh = self._highest
+        sl = self._lowest
+        if isinstance(other, (int, float)):
+            return interval(sl - other, sh - other)
+        elif isinstance(other, interval):
+            oh = other._highest
+            ol = other._lowest
+            return interval(sl - oh, sh - ol)
+    
+    def __rsub__(self, other):
+        return self - other
+    
+    def __mul__
